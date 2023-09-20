c2762e399faa7b32b8346c5baec63363efbe92fc
danyilq <danyil@s2search.io>
2023-09-20 11:27:46 +0200

    Set up expected files structure

---
+++ setup.py
@@ -0,0 +1,54 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf8") as fh:
+    long_description = fh.read()
+
+setup(
+    install_requires=[
+        # difficult packages:
+        "click==8.0.4",
+        # tensor_search:
+        "requests",
+        "urllib3",
+        "fastapi_utils",
+        # s2_inference:
+        "clip-marqo==1.0.2",
+        "more_itertools",
+        "nltk",
+        "torch",
+        "open_clip_torch",
+        "pillow",
+        "numpy",
+        "validators",
+        "sentence-transformers",
+        "onnxruntime-gpu",
+        "onnx",
+        "protobuf==3.20.1",
+        "pandas",
+        "optimum",
+        "fastapi",
+        "uvicorn[standard]",
+        "fastapi_utils",
+        "opencv-python-headless",
+        "psutil",
+        "multilingual_clip"
+    ],
+    name="marqo-commons",
+    version="0.0.1",
+    author="marqo org",
+    author_email="org@marqo.io",
+    description="Commons for marqo projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(where="src", exclude=("tests*",)),
+    keywords="search python marqo opensearch neural tensor semantic vector embedding",
+    platform="any",
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    include_package_data=True,
+    python_requires=">=3",
+    package_dir={"": "src"},
+)
