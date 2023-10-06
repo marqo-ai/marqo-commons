64987109c1ff9861b0d22df3fa3ba8b2570a67a7
Joshua Kim <joshua@marqo.ai>
2023-10-06 14:23:08 +0800

    remove status code from invalid arg error

--- src/marqo_commons/shared_utils/errors.py
+++ src/marqo_commons/shared_utils/errors.py
@@ -1,7 +1,3 @@
-from http import HTTPStatus
-
-
 class InvalidSettingsArgError(Exception):
     code = "invalid_argument"
-    status_code = HTTPStatus.BAD_REQUEST
     error_type = "invalid_request"
